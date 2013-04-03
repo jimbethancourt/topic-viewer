11ef3022fc48d835f91d267abbd5ea0b0747dfc1
gugajansen <gugajansen@9149ecd8-e54b-3d7e-7680-4c9b0e4c2eda>
2013-04-03 17:45:23 +0000

    Fixing bug on topic concentration calculation

--- trunk/TopicViewer/src/br/ufmg/aserg/topicviewer/control/measurement/metrics/TopicConcentration.java
+++ trunk/TopicViewer/src/br/ufmg/aserg/topicviewer/control/measurement/metrics/TopicConcentration.java
@@ -16,14 +16,17 @@ public class TopicConcentration extends AbstractSemanticTopicMetric {
 	}
 
 	private double calculate(String packageName) {
+		
 		Set<Integer> packageTopics = new HashSet<Integer>();
 		double[] topicFrequency = new double[this.numTopics];
+		double numClasses = 0D;
 		
 		List<String> classes = this.distributionMap.getClasses(packageName);
 		for (String className : classes) {
 			int clusterIndex = this.distributionMap.getCluster(className);
 			
 			if (clusterIndex != -1) {
+				numClasses++;
 				packageTopics.add(clusterIndex);
 				topicFrequency[clusterIndex]++;
 			}
@@ -31,11 +34,15 @@ public class TopicConcentration extends AbstractSemanticTopicMetric {
 		
 		Arrays.sort(topicFrequency);
 		double concentration = 0D;
-		double clusterSize = classes.size();
-		double strengthFactor = 1D;
+		
+		int index = -1;
+//		double lastFrequency = 0;
+		double strengthFactor = 0.5;
+		
 		for (int i = topicFrequency.length-1; i >= 0; i--) {
-			concentration += (topicFrequency[i]/clusterSize) * strengthFactor;
-			strengthFactor *= 0.5;
+			index++;
+			if (numClasses > 0) 
+				concentration += (topicFrequency[i]/numClasses) * Math.pow(strengthFactor, index);
 		}
 		
 		return concentration;
