7758af8aba975b13e3bdc8ce11d2f6158595957f
Jim Bethancourt <jimbethancourt@gmail.com>
2016-05-04 09:57:12 -0500

    Reverted first .equals() comparison in Pair.equals()

--- TopicViewer/src/br/ufmg/aserg/topicviewer/control/correlation/clustering/HierarchicalClustering.java
+++ TopicViewer/src/br/ufmg/aserg/topicviewer/control/correlation/clustering/HierarchicalClustering.java
@@ -272,10 +272,10 @@ public class HierarchicalClustering {
 	    	if (other instanceof Pair) {
 	    		Pair otherPair = (Pair) other;
 	    		return 
-	    		((  this.first.equals(otherPair.first) ||
+	    		((  this.first== otherPair.first ||
 	    			( this.first != null && otherPair.first != null &&
 	    			  this.first.equals(otherPair.first))) &&
-	    		 (	this.second.equals(otherPair.second) ||
+	    		 (	this.second == otherPair.second ||
 	    			( this.second != null && otherPair.second != null &&
 	    			  this.second.equals(otherPair.second))) );
 	    	}
